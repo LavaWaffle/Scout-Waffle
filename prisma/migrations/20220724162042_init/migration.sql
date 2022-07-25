2ac291efb56e1bd6a794bcd33258d72c47498951
LavaWaffle <60158215+LavaWaffle@users.noreply.github.com>
2022-07-25 09:34:54 -0400

    removed prisma build files

--- prisma/migrations/20220724162042_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `climbBar` to the `game` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `game` ADD COLUMN `climbBar` ENUM('Low', 'Middle', 'High', 'Traversal') NOT NULL;
