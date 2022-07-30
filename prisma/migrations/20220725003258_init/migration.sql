9bf155b68e815d6a6731eacd911c5fc822b0b0c6
LavaWaffle <60158215+LavaWaffle@users.noreply.github.com>
2022-07-29 23:15:32 -0400

    removed migration files

--- prisma/migrations/20220725003258_init/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - Added the required column `tornament` to the `game` table without a default value. This is not possible if the table is not empty.
-
-*/
--- AlterTable
-ALTER TABLE `game` ADD COLUMN `tornament` VARCHAR(191) NOT NULL;
